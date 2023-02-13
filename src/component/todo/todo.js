9fa821fe5c2d0752e30ee9e48ac32a8077af0ff1
Pratik <guptapratik022@gmail.com>
2023-02-13 16:51:35 +0530

    complete todo project

--- src/component/todo/todo.js
+++ src/component/todo/todo.js
@@ -13,10 +13,25 @@ const Todo = () => {
     }
 
     const[items,setItems] = useState(getLocalData);
+    const[isEditItem , setIsEditItem] = useState("");
+    const[toggleButton,setToggleButton] = useState(false);
 
     const addItem = () =>{
         if(!inputData){
             alert("please enter some data in input field");
+        }else if(inputData && toggleButton){
+            console.log(items);
+            setItems(
+                items.map((curElem) => {
+                    if(curElem.id === isEditItem){
+                        return {...curElem,name:inputData};
+                    }
+                    return curElem;
+                })
+            );
+            setInputData("");
+            setIsEditItem(null);
+            setToggleButton(false);
         }else{
             const myInputData = {
                 id: new Date().getTime().toString(),
@@ -26,6 +41,17 @@ const Todo = () => {
             setInputData("");
         }
     };
+    // console.log(items);
+
+    const editItem = (id) =>{
+        const todo_edited_item = items.find((curElem) => {
+            return curElem.id === id;
+        });
+        setInputData(todo_edited_item.name);
+        setIsEditItem(id);
+        setToggleButton(true);
+    };
+
 
     const deleteItem = (id) =>{
         const updatedItems = items.filter((curelem) => {
@@ -53,7 +79,8 @@ const Todo = () => {
                 <div className="addItems">
                     <input type="text" name="" placeholder='✍ Add Item' id="" value={inputData} 
                     onChange={(event)=>setInputData(event.target.value)} />
-                    <i className="fa fa-plus add-btn" onClick={addItem}></i>
+                    {toggleButton ? (<i className="far fa-edit add-btn" onClick={addItem}></i>) : (<i className="fa fa-plus add-btn" onClick={addItem}></i>)}
+                    {/* <i className="fa fa-plus add-btn" onClick={addItem}></i> */}
                 </div>
                 {/* Show our items */}
                 <div className="showItems">
@@ -62,7 +89,7 @@ const Todo = () => {
                         <div className="eachItem" key={curElement.id}>
                         <h3>{curElement.name}</h3>
                         <div className="todo-btn">
-                        <i className="far fa-edit add-btn"></i>
+                        <i className="far fa-edit add-btn" onClick={()=>editItem(curElement.id)}></i>
                         <i className="far fa-trash-alt add-btn" onClick={()=>deleteItem(curElement.id)}></i>
                         </div>
                     </div> )
